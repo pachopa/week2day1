4f6ed44b9b41bb9401023292395b03082a2798db
chris <pachopa0320@gmail.com>
2017-05-08 19:38:24 +0000

    Printing in Chunks

---
+++ exercise.js
@@ -0,0 +1,26 @@
+// // while https is built-in to Node, it is a module, so it must be required
+var https = require('https');
+
+
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+  https.get(requestOptions, function (response){
+  response.setEncoding('utf8');
+  response.on('data', function(data){
+    console.log('Chunk Received. length:', data + '\n');
+  });
+  response.on('end', function(){
+    console.log('Response stream complete.')
+  })
+  });
+}
+
+
+
+getAndPrintHTMLChunks();
+
